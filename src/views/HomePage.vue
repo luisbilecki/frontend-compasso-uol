72dcc6835d3732bf1581c276e80d59aebdd0edb9
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-11-10 21:30:55 -0300

    feat: added vuejs router in project

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h4>Welcome to the HomePage</h4>
+    <h5>{{ $route.params.username }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {},
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
